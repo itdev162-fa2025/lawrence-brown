9429c4d61c69add5b4fe2a0e7b912db54e5be239
DJLARRYLOVE <browl191@gmatc.matc.edu>
2024-10-08 16:29:06 -0500

    Activity-6, End of p. 9

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -8,7 +8,8 @@ using Microsoft.AspNetCore.Mvc;
 
 using Persistence;
 
-namespace API.Controllers;
+namespace API.Controllers
+{
 [ApiController]
 [Route("api/[controller]")]
 
@@ -36,10 +37,39 @@ namespace API.Controllers;
         [HttpGet("{id}", Name ="GetById")]
         public ActionResult<Post> GetById(Guid id)
         {
-            var post = this._context.Posts.Find(id);            if (post is null)
+            var post = this._context.Posts.Find(id);
+            if (post is null)
             {
                 return NotFound();
             }
         return Ok(post);
-        }   
+        }
+
+        /// <summary>
+        /// POST api/post
+        /// </summary>
+        /// <param name="request"></param>
+        /// <returns>A new post</returns>
+        [HttpPost(Name = "Create")]
+        public ActionResult<Post> Create([FromBody]Post request)
+        {
+            var post = new Post
+            {
+                Id = request.Id,
+                Title = request.Title,
+                Body = request.Body,
+                Date = request.Date
+            };
+
+            _context.Posts.Add(post);
+            var success = _context.SaveChanges() > 0;
+
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception ("Error creating post");
+        }
+    }
 }

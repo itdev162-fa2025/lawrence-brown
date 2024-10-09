4080796596118a4275e6b9947bdf2aba1bdede3d
DJLARRYLOVE <browl191@gmatc.matc.edu>
2024-10-09 17:49:51 -0500

    Activity 6, p 13

--- API/Controllers/PostsController.cs
+++ API/Controllers/PostsController.cs
@@ -71,5 +71,33 @@ namespace API.Controllers
               
             throw new Exception ("Error creating post");
         }
+
+        /// <summary>
+        /// PUT api/post
+        /// </summary>
+        /// <param name="request">JSON request containing one or more updated post fields</param>
+        /// <returns></returns>
+        [HttpPut(Name = "Update")]
+        public ActionResult<Post> Update([FromBody]Post request)
+        {
+            var post = _context.Posts.Find(request.Id);
+            if (post == null)
+            {
+                throw new Exception("Could not find post");
+            }
+
+            //Update the post properties with request values, if present
+            post.Title = request.Title != null ? request.Title : post.Title;
+            post.Body = request.Body != null ? request.Body : post.Body;
+            post.Date = request.Date != DateTime.MinValue ? request.Date : post.Date;
+
+            var success = _context.SaveChanges() > 0;
+            if (success)
+            {
+                return Ok(post);
+            }
+
+            throw new Exception("Error updating post");
+        }
     }
 }

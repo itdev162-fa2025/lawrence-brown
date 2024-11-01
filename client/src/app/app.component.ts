048fcd703176af5eae9305c192b8a1bc47d76e5f
DJLARRYLOVE <browl191@gmatc.matc.edu>
2024-10-31 22:32:40 -0500

    Added Met Card Additions and Changes

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,11 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [RouterOutlet, 
+    CommonModule,
+    MatCardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
